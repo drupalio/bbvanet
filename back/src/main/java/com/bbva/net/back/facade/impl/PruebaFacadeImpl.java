ade4452de0c90540777777ff2a301dbb8052cf0e
Jhon <jhon@entelgy.com>
2014-12-04 16:59:12 +0000

    agrega detalle cuenta

--- back/src/main/java/com/bbva/net/back/facade/impl/PruebaFacadeImpl.java
+++ back/src/main/java/com/bbva/net/back/facade/impl/PruebaFacadeImpl.java
@@ -2,12 +2,10 @@ package com.bbva.net.back.facade.impl;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.math.BigDecimal;
 
 import javax.annotation.Resource;
 
 import co.com.bbva.services.transactions.globalposition.schema.Account;
-import co.com.bbva.services.transactions.globalposition.schema.GlobalProducts;
 import co.com.bbva.services.transactions.globalposition.schema.Product;
 
 import com.bbva.net.back.core.pattern.AbstractBbvaFacade;
@@ -19,20 +17,19 @@ import com.bbva.net.back.facade.PruebaFacade;
 import com.google.gson.Gson;
 
 @Facade(value = "pruebaFacade")
-public class PruebaFacadeImpl 
-				extends AbstractBbvaFacade implements PruebaFacade {
+public class PruebaFacadeImpl extends AbstractBbvaFacade implements
+		PruebaFacade {
 
 	private String cadena;
 
-	@Resource(name="pruebaDao")
+	@Resource(name = "pruebaDao")
 	private transient PruebaDao pruebaDao;
-	
-	
-	@Resource(name="globalPositionFacade")
+
+	@Resource(name = "globalPositionFacade")
 	private transient GlobalPositionFacade globalPositionFacade;
-	
-	public PruebaFacadeImpl(){
-		this.cadena="LUIS";
+
+	public PruebaFacadeImpl() {
+		this.cadena = "LUIS";
 	}
 
 	public String getCadena() {
@@ -53,41 +50,37 @@ public class PruebaFacadeImpl
 		return prueba;
 	}
 
-	
-	
-	public static void main (final String[] args){
-		
-		
+	public static void main(final String[] args) {
+
 		Account obj = new Account();
-		obj.setOverDraft(BigDecimal.ONE);
+		// obj.setOverDraft(BigDecimal.ONE);
 		Product product = new Product();
 		product.setAlias("hola");
 		product.setAsset(true);
-		product.setCashAvailable(BigDecimal.ONE);
+		// product.setCashAvailable(BigDecimal.ONE);
 		product.setProductId("dasfa");
 		product.setProductName("hohooas");
 		product.setProductNumber("123113213");
-		product.setTotalCash(BigDecimal.ONE);
+		// product.setTotalCash(BigDecimal.ONE);
 		obj.setProduct(product);
 		Gson gson = new Gson();
-	 
+
 		// convert java object to JSON format,
 		// and returned as JSON formatted string
 		String json = gson.toJson(obj);
-	 
+
 		try {
-			//write converted json data to a file named "file.json"
+			// write converted json data to a file named "file.json"
 			FileWriter writer = new FileWriter("c:\\bin\\file.json");
 			writer.write(json);
 			writer.close();
-	 
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	 
+
 		System.out.println(json);
-	 
-		
+
 	}
-	
+
 }
