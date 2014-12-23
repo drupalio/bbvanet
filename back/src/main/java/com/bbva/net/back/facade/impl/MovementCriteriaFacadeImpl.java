17a166ce280cc1c1a57b252ef69ce2e5a84edb1f
Aleja195 <alejandra.gutierrez@entelgy.com>
2014-12-23 17:10:49 -0500

    confilctos

---
+++ back/src/main/java/com/bbva/net/back/facade/impl/MovementCriteriaFacadeImpl.java
@@ -0,0 +1,12 @@
+package com.bbva.net.back.facade.impl;
+
+import com.bbva.net.back.core.pattern.facade.AbstractBbvaFacade;
+import com.bbva.net.back.core.stereotype.Facade;
+import com.bbva.net.back.facade.MovementCriteriaFacade;
+
+@Facade(value = "movementCriteriaFacade")
+public class MovementCriteriaFacadeImpl extends AbstractBbvaFacade implements MovementCriteriaFacade {
+
+	private static final long serialVersionUID = 1L;
+
+}
