6a0b716bb071febf67c279ae9a198b7303ed7ed4
fgparamio <fgparamio@yahoo.es>
2015-01-06 15:18:48 -0500

    Webservies Loan generate and comment errors generation

--- webservices/src/main/java/com/bbva/czic/dto/net/Loan.java
+++ webservices/src/main/java/com/bbva/czic/dto/net/Loan.java
@@ -1,20 +1,18 @@
-
 package com.bbva.czic.dto.net;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElementRef;
-import javax.xml.bind.annotation.XmlElementRefs;
 import javax.xml.bind.annotation.XmlType;
 
-
 /**
- * <p>Clase Java para Loan complex type.
- * 
- * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * <p>
+ * Clase Java para Loan complex type.
+ * <p>
+ * El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
  * 
  * <pre>
  * &lt;complexType name="Loan">
@@ -29,63 +27,47 @@ import javax.xml.bind.annotation.XmlType;
  *   &lt;/complexContent>
  * &lt;/complexType>
  * </pre>
- * 
- * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "Loan", propOrder = {
-    "rest"
-})
-public class Loan
-    extends Product
-{
+@XmlType(name = "Loan", propOrder = { "rest" })
+public class Loan extends Product {
 
-    @XmlElementRefs({
-        @XmlElementRef(name = "balance", type = JAXBElement.class, required = false),
-        @XmlElementRef(name = "payment", type = JAXBElement.class, required = false),
-        @XmlElementRef(name = "status", type = JAXBElement.class, required = false)
-    })
-    protected List<JAXBElement<?>> rest;
+	// @XmlElementRefs({
+	// @XmlElementRef(name = "balance", type = JAXBElement.class, required = false),
+	// @XmlElementRef(name = "payment", type = JAXBElement.class, required = false),
+	// @XmlElementRef(name = "status", type = JAXBElement.class, required = false)
+	// })
+	protected List<JAXBElement<?>> rest;
 
-    /**
-     * Obtiene el resto del modelo de contenido. 
-     * 
-     * <p>
-     * Ha obtenido esta propiedad que permite capturar todo por el siguiente motivo: 
-     * El nombre de campo "Balance" se está utilizando en dos partes diferentes de un esquema. Consulte: 
-     * línea 0 de file:/C:/Jhon/Entelgy/BLUE%20SPRING/WS/bbvaNet/bbvanet/webservices/src/main/resources/wadl/loan.xml#grammar1
-     * línea 0 de file:/C:/Jhon/Entelgy/BLUE%20SPRING/WS/bbvaNet/bbvanet/webservices/src/main/resources/wadl/loan.xml#grammar1
-     * <p>
-     * Para deshacerse de esta propiedad, aplique una personalización de propiedad a una
-     * de las dos declaraciones siguientes para cambiarles de nombre: 
-     * Gets the value of the rest property.
-     * 
-     * <p>
-     * This accessor method returns a reference to the live list,
-     * not a snapshot. Therefore any modification you make to the
-     * returned list will be present inside the JAXB object.
-     * This is why there is not a <CODE>set</CODE> method for the rest property.
-     * 
-     * <p>
-     * For example, to add a new item, do as follows:
-     * <pre>
-     *    getRest().add(newItem);
-     * </pre>
-     * 
-     * 
-     * <p>
-     * Objects of the following type(s) are allowed in the list
-     * {@link JAXBElement }{@code <}{@link Balance }{@code >}
-     * {@link JAXBElement }{@code <}{@link Payment }{@code >}
-     * {@link JAXBElement }{@code <}{@link EnumLoanStatus }{@code >}
-     * 
-     * 
-     */
-    public List<JAXBElement<?>> getRest() {
-        if (rest == null) {
-            rest = new ArrayList<JAXBElement<?>>();
-        }
-        return this.rest;
-    }
+	/**
+	 * Obtiene el resto del modelo de contenido.
+	 * <p>
+	 * Ha obtenido esta propiedad que permite capturar todo por el siguiente motivo: El nombre de campo "Balance" se estï¿½
+	 * utilizando en dos partes diferentes de un esquema. Consulte: lï¿½nea 0 de
+	 * file:/C:/Jhon/Entelgy/BLUE%20SPRING/WS/bbvaNet/bbvanet/webservices/src/main/resources/wadl/loan.xml#grammar1 lï¿½nea 0
+	 * de file:/C:/Jhon/Entelgy/BLUE%20SPRING/WS/bbvaNet/bbvanet/webservices/src/main/resources/wadl/loan.xml#grammar1
+	 * <p>
+	 * Para deshacerse de esta propiedad, aplique una personalizaciï¿½n de propiedad a una de las dos declaraciones siguientes
+	 * para cambiarles de nombre: Gets the value of the rest property.
+	 * <p>
+	 * This accessor method returns a reference to the live list, not a snapshot. Therefore any modification you make to the
+	 * returned list will be present inside the JAXB object. This is why there is not a <CODE>set</CODE> method for the rest
+	 * property.
+	 * <p>
+	 * For example, to add a new item, do as follows:
+	 * 
+	 * <pre>
+	 * getRest().add(newItem);
+	 * </pre>
+	 * <p>
+	 * Objects of the following type(s) are allowed in the list {@link JAXBElement }{@code <}{@link Balance }{@code >}
+	 * {@link JAXBElement }{@code <}{@link Payment }{@code >} {@link JAXBElement }{@code <}{@link EnumLoanStatus }{@code >}
+	 */
+	public List<JAXBElement<?>> getRest() {
+		if (rest == null) {
+			rest = new ArrayList<JAXBElement<?>>();
+		}
+		return this.rest;
+	}
 
 }
