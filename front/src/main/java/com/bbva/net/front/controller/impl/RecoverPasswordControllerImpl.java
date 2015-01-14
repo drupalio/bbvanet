185d5d21b0aa86585ea36c1a8adf59872aef8ec0
ivan.trebilcock <ivan.trebilcock@entelgy.com>
2015-01-14 17:40:22 -0500

    Flow recovery password

--- front/src/main/java/com/bbva/net/front/controller/impl/RecoverPasswordControllerImpl.java
+++ front/src/main/java/com/bbva/net/front/controller/impl/RecoverPasswordControllerImpl.java
@@ -3,12 +3,12 @@ package com.bbva.net.front.controller.impl;
 import org.springframework.stereotype.Controller;
 
 import com.bbva.net.back.model.UserData.ForgotPasswordDto;
-import com.bbva.net.front.controller.UserRecoverPassword;
+import com.bbva.net.front.controller.RecoverPasswordController;
 import com.bbva.net.front.core.AbstractBbvaController;
 
-@Controller(value = "userRecoverPassword")
-public class UserRecoverPasswordImpl extends AbstractBbvaController implements
-		UserRecoverPassword {
+@Controller(value = "recoverPassController")
+public class RecoverPasswordControllerImpl extends AbstractBbvaController implements
+		RecoverPasswordController {
 	private static final long serialVersionUID = 6795761532672076491L;
 
 	private ForgotPasswordDto forgotPasswordDto;
@@ -29,4 +29,10 @@ public class UserRecoverPasswordImpl extends AbstractBbvaController implements
 	public void setForgotPasswordDto(ForgotPasswordDto forgotPasswordDto) {
 		this.forgotPasswordDto = forgotPasswordDto;
 	}
+	
+	
+	public String respuesta(){
+		System.out.println("Respuesta ");
+		return "start";
+	} 
 }
