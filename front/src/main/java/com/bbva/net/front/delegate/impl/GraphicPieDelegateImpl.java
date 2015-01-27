2fbe1d43be29e3ea4a7e941df6e6c8a4758722b1
mpacheco <mario.pacheco@entelgy.com>
2015-01-27 09:29:43 -0500

    Ajustes Currency a las gráficas tu situación

--- front/src/main/java/com/bbva/net/front/delegate/impl/GraphicPieDelegateImpl.java
+++ front/src/main/java/com/bbva/net/front/delegate/impl/GraphicPieDelegateImpl.java
@@ -44,24 +44,18 @@ public class GraphicPieDelegateImpl implements GraphicPieDelegate {
 		assetPie.setHeaderCenter("Pesos ($)");
 		final List<PieItemUI> assetPieItems = new ArrayList<PieItemUI>();
 
-		final PieItemUI salesPieItem = new PieItemUI("#1874CD",
-				MessagesHelper.INSTANCE.getString("categorie.sales"),
+		final PieItemUI salesPieItem = new PieItemUI("#1874CD", MessagesHelper.INSTANCE.getString("categorie.sales"),
 				cardsCharges.get(0).getAmmount().getAmount());
 		final PieItemUI clothesPieItem = new PieItemUI("#104E8B",
-				MessagesHelper.INSTANCE.getString("categorie.clothes"),
-				cardsCharges.get(1).getAmmount().getAmount());
-		final PieItemUI othersPieItem = new PieItemUI("#A2CD5A",
-				MessagesHelper.INSTANCE.getString("categorie.others"),
+				MessagesHelper.INSTANCE.getString("categorie.clothes"), cardsCharges.get(1).getAmmount().getAmount());
+		final PieItemUI othersPieItem = new PieItemUI("#A2CD5A", MessagesHelper.INSTANCE.getString("categorie.others"),
 				cardsCharges.get(2).getAmmount().getAmount());
 		final PieItemUI leisurePieItem = new PieItemUI("#698B22",
-				MessagesHelper.INSTANCE.getString("categorie.leisure"),
-				cardsCharges.get(3).getAmmount().getAmount());
-		final PieItemUI booksPieItem = new PieItemUI("#FFC125",
-				MessagesHelper.INSTANCE.getString("categorie.books"),
+				MessagesHelper.INSTANCE.getString("categorie.leisure"), cardsCharges.get(3).getAmmount().getAmount());
+		final PieItemUI booksPieItem = new PieItemUI("#FFC125", MessagesHelper.INSTANCE.getString("categorie.books"),
 				cardsCharges.get(4).getAmmount().getAmount());
 		final PieItemUI commercePieItem = new PieItemUI("#EE7600",
-				MessagesHelper.INSTANCE.getString("categorie.commerce"),
-				cardsCharges.get(5).getAmmount().getAmount());
+				MessagesHelper.INSTANCE.getString("categorie.commerce"), cardsCharges.get(5).getAmmount().getAmount());
 
 		assetPieItems.add(salesPieItem);
 		assetPieItems.add(clothesPieItem);
@@ -75,21 +69,17 @@ public class GraphicPieDelegateImpl implements GraphicPieDelegate {
 	}
 
 	@Override
-	public SituationPiesUI getSituationGlobalProducts(
-			final GlobalProductsDto globalProducts) {
+	public SituationPiesUI getSituationGlobalProducts(final GlobalProductsDto globalProducts) {
 
 		final SituationPiesUI situationPiesUI = new SituationPiesUI();
-		final List<ProductDto> productList = productService
-				.getProducts(globalProducts);
+		final List<ProductDto> productList = productService.getProducts(globalProducts);
 
 		situationPiesUI.setSituation(getSituationPieConfig(productList));
 		situationPiesUI.setAssets(getAssetPieConfig(productList));
 		situationPiesUI.setFinancing(getFinanciationPieConfig(productList));
 
-		situationPiesUI.setTotalAssets((productService
-				.getTotalAssets(productList)));
-		situationPiesUI.setTotalFinancing((productService
-				.getTotalFinanciacion(productList)));
+		situationPiesUI.setTotalAssets((productService.getTotalAssets(productList)));
+		situationPiesUI.setTotalFinancing((productService.getTotalFinanciacion(productList)));
 
 		return situationPiesUI;
 	}
@@ -97,8 +87,7 @@ public class GraphicPieDelegateImpl implements GraphicPieDelegate {
 	/**
 	 * Method to draws a Situation Pie graphic
 	 * 
-	 * @param List
-	 *            <Product> products
+	 * @param List <Product> products
 	 * @return PieConfigUI
 	 */
 	public PieConfigUI getSituationPieConfig(final List<ProductDto> products) {
@@ -107,11 +96,10 @@ public class GraphicPieDelegateImpl implements GraphicPieDelegate {
 
 		final List<PieItemUI> situationPieItems = new ArrayList<PieItemUI>();
 
-		final PieItemUI assetPieItem = new PieItemUI("el color", "Activos",
-				this.productService.getTotalAssets(products).getAmount());
-		final PieItemUI financiationPieItem = new PieItemUI("el color",
-				"Financiación", this.productService.getTotalFinanciacion(
-						products).getAmount());
+		final PieItemUI assetPieItem = new PieItemUI("el color", "Activos", this.productService
+				.getTotalAssets(products).getAmount());
+		final PieItemUI financiationPieItem = new PieItemUI("el color", "Financiación", this.productService
+				.getTotalFinanciacion(products).getAmount());
 		situationPieItems.add(assetPieItem);
 		situationPieItems.add(financiationPieItem);
 		situationPie.setPieItemUIList(situationPieItems);
@@ -123,36 +111,32 @@ public class GraphicPieDelegateImpl implements GraphicPieDelegate {
 	/**
 	 * Method to draws a Situation Asset Pie graphic
 	 * 
-	 * @param List
-	 *            <Product> products
+	 * @param List <Product> products
 	 * @return PieConfigUI
 	 */
 	public PieConfigUI getAssetPieConfig(final List<ProductDto> products) {
 
 		final PieConfigUI assetPie = new PieConfigUI();
-		assetPie.setHeaderLeft(" Activos ");
-		assetPie.setHeaderRight(productService.getTotalAssets(products)
-				.getAmount().toString());
+		DecimalFormat myFormatter = new DecimalFormat(ResourceBundle.getBundle("i18n_es").getString(
+				"number.format.decimals"));
+		String totalAssets = myFormatter.format(productService.getTotalAssets(products).getAmount());
 
+		assetPie.setHeaderLeft(" Activos ");
+		assetPie.setHeaderRight(totalAssets);
+		assetPie.setHeaderCenter("COP");
 		final List<PieItemUI> assetPieItems = new ArrayList<PieItemUI>();
 
-		final PieItemUI accountPieItem = new PieItemUI("el color",
-				"Cuentas Personales", this.productService
-						.getTotalProductsByType(products, EnumProductType.PC)
-						.getAmount());
+		final PieItemUI accountPieItem = new PieItemUI("el color", "Cuentas Personales", this.productService
+				.getTotalProductsByType(products, EnumProductType.PC).getAmount());
 
-		final PieItemUI fundPieItem = new PieItemUI("el color",
-				"Fondos de Inversión", this.productService
-						.getTotalProductsByType(products, EnumProductType.SI)
-						.getAmount());
+		final PieItemUI fundPieItem = new PieItemUI("el color", "Fondos de Inversión", this.productService
+				.getTotalProductsByType(products, EnumProductType.SI).getAmount());
 
-		final PieItemUI depositPieItem = new PieItemUI("el color", "Depósitos",
-				this.productService.getTotalProductsByType(products,
-						EnumProductType.ED).getAmount());
+		final PieItemUI depositPieItem = new PieItemUI("el color", "Depósitos", this.productService
+				.getTotalProductsByType(products, EnumProductType.ED).getAmount());
 
-		final PieItemUI rotatingAccountPieItem = new PieItemUI("el color",
-				"Cupo Rotativo", this.productService.getTotalProductsByType(
-						products, EnumProductType.RQ).getAmount());
+		final PieItemUI rotatingAccountPieItem = new PieItemUI("el color", "Cupo Rotativo", this.productService
+				.getTotalProductsByType(products, EnumProductType.RQ).getAmount());
 
 		assetPieItems.add(accountPieItem);
 		assetPieItems.add(fundPieItem);
@@ -167,35 +151,28 @@ public class GraphicPieDelegateImpl implements GraphicPieDelegate {
 	/**
 	 * Method to draws a Situation Asset Pie graphic
 	 * 
-	 * @param List
-	 *            <Product> products
+	 * @param List <Product> products
 	 * @return PieConfigUI
 	 */
 	public PieConfigUI getFinanciationPieConfig(final List<ProductDto> products) {
 
 		final PieConfigUI financiationPie = new PieConfigUI();
-		DecimalFormat myFormatter = new DecimalFormat(ResourceBundle.getBundle(
-				"i18n_es").getString("number.format.decimals"));
-		String str = myFormatter.format(productService.getTotalFinanciacion(
-				products).getAmount());
-		System.out.println(str);
-		financiationPie.setHeaderCenter(productService
-				.getTotalFinanciacion(products).getAmount().toString());
+		DecimalFormat myFormatter = new DecimalFormat(ResourceBundle.getBundle("i18n_es").getString(
+				"number.format.decimals"));
+		String totalFinancing = myFormatter.format(productService.getTotalFinanciacion(products).getAmount());
+		financiationPie.setHeaderCenter(" COP ");
+		financiationPie.setHeaderRight(totalFinancing);
 
 		final List<PieItemUI> financiationPieItems = new ArrayList<PieItemUI>();
 
-		final PieItemUI cardsPieItem = new PieItemUI("el color",
-				"Tarjetas de Crédito", this.productService
-						.getTotalProductsByType(products, EnumProductType.TC)
-						.getAmount());
+		final PieItemUI cardsPieItem = new PieItemUI("el color", "Tarjetas de Crédito", this.productService
+				.getTotalProductsByType(products, EnumProductType.TC).getAmount());
 
-		final PieItemUI leasingPieItem = new PieItemUI("el color", "Leasing",
-				this.productService.getTotalProductsByType(products,
-						EnumProductType.LI).getAmount());
+		final PieItemUI leasingPieItem = new PieItemUI("el color", "Leasing", this.productService
+				.getTotalProductsByType(products, EnumProductType.LI).getAmount());
 
-		final PieItemUI fundsPieItem = new PieItemUI("el color", "Préstamos",
-				this.productService.getTotalProductsByType(products,
-						EnumProductType.LO).getAmount());
+		final PieItemUI fundsPieItem = new PieItemUI("el color", "Préstamos", this.productService
+				.getTotalProductsByType(products, EnumProductType.LO).getAmount());
 
 		financiationPieItems.add(cardsPieItem);
 		financiationPieItems.add(leasingPieItem);
@@ -213,14 +190,11 @@ public class GraphicPieDelegateImpl implements GraphicPieDelegate {
 		final List<PieItemUI> fundsPieItems = new ArrayList<PieItemUI>();
 
 		final PieItemUI valorPieItem = new PieItemUI("#197AC4",
-				MessagesHelper.INSTANCE.getString("graphicFunds.plusValue"),
-				this.fundsService.getTotalFundByType(funds,
-						EnumFundsType.plusValue).getAmount());
+				MessagesHelper.INSTANCE.getString("graphicFunds.plusValue"), this.fundsService.getTotalFundByType(
+						funds, EnumFundsType.plusValue).getAmount());
 		final PieItemUI garantPieItem = new PieItemUI("#83C030",
-				MessagesHelper.INSTANCE
-						.getString("graphicFunds.guaranteedValue"),
-				this.fundsService.getTotalFundByType(funds,
-						EnumFundsType.guaranteedValue).getAmount());
+				MessagesHelper.INSTANCE.getString("graphicFunds.guaranteedValue"), this.fundsService
+						.getTotalFundByType(funds, EnumFundsType.guaranteedValue).getAmount());
 
 		fundsPieItems.add(garantPieItem);
 		fundsPieItems.add(valorPieItem);
@@ -230,8 +204,7 @@ public class GraphicPieDelegateImpl implements GraphicPieDelegate {
 	}
 
 	/**
-	 * @param productService
-	 *            the productService to set
+	 * @param productService the productService to set
 	 */
 	public void setProductService(final ProductService productService) {
 		this.productService = productService;
