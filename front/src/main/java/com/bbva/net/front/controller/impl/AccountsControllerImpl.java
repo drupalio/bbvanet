5df895949e280a33adeb78f415753a6109c77834
fernando <fgparamio@yahoo.es>
2015-01-29 19:47:08 +0000

    Generater first EAR bbvaNet application

--- front/src/main/java/com/bbva/net/front/controller/impl/AccountsControllerImpl.java
+++ front/src/main/java/com/bbva/net/front/controller/impl/AccountsControllerImpl.java
@@ -6,7 +6,6 @@ import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 
 import org.primefaces.event.SelectEvent;
-import org.springframework.stereotype.Controller;
 
 import com.bbva.net.back.facade.AccountsFacade;
 import com.bbva.net.back.facade.TermasAccountsFacade;
@@ -15,7 +14,6 @@ import com.bbva.net.back.model.globalposition.AccountDto;
 import com.bbva.net.front.controller.AccountsController;
 import com.bbva.net.front.core.AbstractBbvaController;
 
-@Controller(value = "accountsController")
 public class AccountsControllerImpl extends AbstractBbvaController implements AccountsController {
 
 	private static final long serialVersionUID = 5726824668267606699L;
