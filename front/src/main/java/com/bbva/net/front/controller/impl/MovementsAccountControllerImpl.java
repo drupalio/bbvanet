e9b2cb09fde2fe83fca5b276b4a12129519a62a6
fgparamio <fgparamio@yahoo.es>
2015-02-07 16:25:31 -0500

    War Was 8.0 Deployed

--- front/src/main/java/com/bbva/net/front/controller/impl/MovementsAccountControllerImpl.java
+++ front/src/main/java/com/bbva/net/front/controller/impl/MovementsAccountControllerImpl.java
@@ -1,15 +1,11 @@
 package com.bbva.net.front.controller.impl;
 
 import javax.annotation.PostConstruct;
-import javax.faces.bean.ManagedBean;
-import javax.faces.view.ViewScoped;
 
 import com.bbva.net.back.model.globalposition.ProductDto;
 import com.bbva.net.front.controller.MovementsAccountController;
 import com.bbva.net.front.core.AbstractBbvaController;
 
-@ManagedBean
-@ViewScoped
 public class MovementsAccountControllerImpl extends AbstractBbvaController implements MovementsAccountController {
 
 	private static final long serialVersionUID = 2246759826282199706L;
