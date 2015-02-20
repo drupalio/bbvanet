f3bf5e30841e791c13edbd71ed190b456e18063a
Nerly <nerly.aguilera@entelgy.com>
2015-02-20 09:30:28 -0500

    Todos los ajustes reportados

--- front/src/main/java/com/bbva/net/front/controller/impl/FavoriteOperationsControllerImpl.java
+++ front/src/main/java/com/bbva/net/front/controller/impl/FavoriteOperationsControllerImpl.java
@@ -8,6 +8,8 @@ import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 
 import org.primefaces.event.SelectEvent;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
 
 import com.bbva.net.back.facade.FavoriteOperationsFacade;
 import com.bbva.net.back.model.favoriteOperations.FavoriteOperationDto;
@@ -15,6 +17,11 @@ import com.bbva.net.front.controller.FavoriteOperationsController;
 import com.bbva.net.front.core.AbstractBbvaController;
 import com.bbva.net.front.helper.MessagesHelper;
 
+/**
+ * @author Entelgy
+ */
+@Controller(value = "favoriteOperationsController")
+@Scope(value = "globalSession")
 public class FavoriteOperationsControllerImpl extends AbstractBbvaController implements FavoriteOperationsController {
 
 	/**
@@ -27,7 +34,6 @@ public class FavoriteOperationsControllerImpl extends AbstractBbvaController imp
 	@Resource(name = "favoriteOperationsFacade")
 	private transient FavoriteOperationsFacade favoriteOperationsFacade;
 
-	@PostConstruct
 	public void init() {
 		favoriteOperations = favoriteOperationsFacade.getListFavoriteOperations();
 
@@ -41,18 +47,43 @@ public class FavoriteOperationsControllerImpl extends AbstractBbvaController imp
 
 	@Override
 	public List<FavoriteOperationDto> getListFavoriteOperations() {
-		return favoriteOperations.subList(0, 3);
+
+		if (favoriteOperations.size() <= 3) {
+			return favoriteOperations;
+		} else {
+			return favoriteOperations.subList(0, 3);
+		}
 	}
 
 	@Override
 	public List<FavoriteOperationDto> getListFavoriteOperationsHidden() {
-		return favoriteOperations.subList(3, favoriteOperations.size());
+
+		if (favoriteOperations.size() <= 3) {
+			return favoriteOperations;
+		} else {
+			return favoriteOperations.subList(3, favoriteOperations.size());
+		}
 	}
 
 	public String getDate(Date transactionDate) {
-		SimpleDateFormat dateFormat = new SimpleDateFormat(
+		final SimpleDateFormat dateFormat = new SimpleDateFormat(
 				MessagesHelper.INSTANCE.getStringI18("date.pattner.dd-mm-yyyy"));
 		return dateFormat.format(transactionDate);
+
+	}
+
+	/**
+     * 
+     */
+	public void getNames() {
+		for (int i = 0; i < favoriteOperations.size(); i++) {
+			final String origen = MessagesHelper.INSTANCE.getFavOperationsPrefix(this.favoriteOperations.get(i)
+					.getOrigin());
+			final String destino = MessagesHelper.INSTANCE.getFavOperationsPrefix(this.favoriteOperations.get(i)
+					.getDestination());
+			this.favoriteOperations.get(i).setOrigin(origen);
+			this.favoriteOperations.get(i).setDestination(destino);
+		}
 
 	}
 
@@ -62,6 +93,14 @@ public class FavoriteOperationsControllerImpl extends AbstractBbvaController imp
 
 	public void setFavoriteOperations(List<FavoriteOperationDto> favoriteOperations) {
 		this.favoriteOperations = favoriteOperations;
+	}
+
+	public FavoriteOperationsFacade getFavoriteOperationsFacade() {
+		return favoriteOperationsFacade;
+	}
+
+	public void setFavoriteOperationsFacade(FavoriteOperationsFacade favoriteOperationsFacade) {
+		this.favoriteOperationsFacade = favoriteOperationsFacade;
 	}
 
 }
--- front/src/main/java/com/bbva/net/front/controller/impl/GlobalPositionControllerImpl.java
+++ front/src/main/java/com/bbva/net/front/controller/impl/GlobalPositionControllerImpl.java
@@ -155,8 +155,14 @@ public class GlobalPositionControllerImpl extends AbstractBbvaController impleme
 	 */
 	private String accountSelected = StringUtils.EMPTY;
 
+	/**
+	 * periodo selecionado en grafica de tarjetas
+	 */
 	private String periodCardSelected = StringUtils.EMPTY;
 
+	/**
+	 * producto seleccionado en grafica de tarjetas
+	 */
 	private String cardSelected = StringUtils.EMPTY;
 
 	/**
@@ -408,6 +414,21 @@ public class GlobalPositionControllerImpl extends AbstractBbvaController impleme
 
 	}
 
+	/**
+	 * Enmascara el número de tarjeta
+	 * 
+	 * @param number
+	 * @return
+	 */
+	public String maskCardsNumber(final String number) {
+		String mask = "";
+		for (int i = 0; i < number.length() - 4; i++) {
+			if (i % 4 == 0) mask += " ";
+			mask += "*";
+		}
+		return mask + " " + number.substring(number.length() - 4, number.length());
+	}
+
 	/************************************* SETTER BEANS **************************************/
 
 	/**
@@ -537,10 +558,16 @@ public class GlobalPositionControllerImpl extends AbstractBbvaController impleme
 		this.accountMonthBalanceFacade = accountMonthBalanceFacade;
 	}
 
+	/**
+	 * @return globalMonthlyBalance
+	 */
 	public GlobalMonthlyBalanceDto getGlobalMonthlyBalance() {
 		return globalMonthlyBalance;
 	}
 
+	/**
+	 * @return lineConfigUI
+	 */
 	public LineConfigUI getLineConfigUI() {
 		return lineConfigUI;
 	}
