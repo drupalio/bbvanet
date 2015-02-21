95c279c8cdf7bfefb2401cf96e082c5e16e37b91
mpacheco <mario.pacheco@entelgy.com>
2015-02-21 10:32:59 -0500

    Extracto Render Mes en construcción

--- front/src/main/java/com/bbva/net/front/controller/ExtractController.java
+++ front/src/main/java/com/bbva/net/front/controller/ExtractController.java
@@ -1,10 +1,9 @@
 package com.bbva.net.front.controller;
 
 import java.util.List;
-import java.util.Map;
 
 public interface ExtractController {
 
-	Map<String, List<String>> getExtractAvailablePeriod();
+	List<String> getExtractAvailablePeriod();
 
 }
--- front/src/main/java/com/bbva/net/front/controller/impl/ExtractControllerImpl.java
+++ front/src/main/java/com/bbva/net/front/controller/impl/ExtractControllerImpl.java
@@ -1,10 +1,13 @@
 package com.bbva.net.front.controller.impl;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.beanutils.BeanToPropertyValueTransformer;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 
 import com.bbva.net.back.facade.ExtractFacade;
@@ -28,16 +31,45 @@ public class ExtractControllerImpl extends AbstractBbvaController implements Ext
 
 	private List<ExtractDto> extractList;
 
-	@Override
-	public Map<String, List<String>> getExtractAvailablePeriod() {
+	private List<String> yearAvailable;
 
-		return null;
+	private List<String> monthAvailable;
+
+	private boolean enableMonth = true;
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<String> getExtractAvailablePeriod() {
+
+		this.yearAvailable = (List<String>)CollectionUtils.collect(extractList, new BeanToPropertyValueTransformer(
+				"year"));
+
+		this.monthAvailable = (List<String>)CollectionUtils.collect(extractList, new BeanToPropertyValueTransformer(
+				"month"));
+
+		yearAvailable = new ArrayList<String>(new LinkedHashSet<String>(yearAvailable));
+
+		monthAvailable = new ArrayList<String>(new LinkedHashSet<String>(monthAvailable));
+
+		return yearAvailable;
+	}
+
+	public void actionState() {
+		enableMonth = true;
+		getExtractMontAvailable();
+	}
+
+	public List<String> getExtractMontAvailable() {
+
+		return monthAvailable;
 	}
 
 	public void init() {
 
 		this.extractList = this.extractFacade.getExtractAvailablePeriod(super.getSelectedProduct().getProductNumber(),
 				StringUtils.EMPTY);
+
+		getExtractAvailablePeriod();
 
 	}
 
@@ -63,6 +95,26 @@ public class ExtractControllerImpl extends AbstractBbvaController implements Ext
 
 	public void setExtractFacade(final ExtractFacade extractFacade) {
 		this.extractFacade = extractFacade;
+	}
+
+	public List<String> getYearAvailable() {
+		return yearAvailable;
+	}
+
+	public List<String> getMonthAvailable() {
+		return monthAvailable;
+	}
+
+	public void setMonthAvailable(List<String> monthAvailable) {
+		this.monthAvailable = monthAvailable;
+	}
+
+	public boolean isEnableMonth() {
+		return enableMonth;
+	}
+
+	public void setEnableMonth(boolean enableMonth) {
+		this.enableMonth = enableMonth;
 	}
 
 }
