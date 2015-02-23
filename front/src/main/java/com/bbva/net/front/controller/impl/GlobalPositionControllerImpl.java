d055edf385d368f093d4339dc304c357d0de0ea0
mpacheco <mario.pacheco@entelgy.com>
2015-02-23 17:03:35 -0500

    Ajustes a Test de GP

--- front/src/main/java/com/bbva/net/front/controller/impl/GlobalPositionControllerImpl.java
+++ front/src/main/java/com/bbva/net/front/controller/impl/GlobalPositionControllerImpl.java
@@ -19,7 +19,6 @@ import com.bbva.net.back.model.commons.DateRangeDto;
 import com.bbva.net.back.model.globalposition.BalanceDto;
 import com.bbva.net.back.model.globalposition.GlobalProductsDto;
 import com.bbva.net.back.model.movements.GlobalResumeMovementsDto;
-import com.bbva.net.back.service.DateFilterService;
 import com.bbva.net.back.service.impl.DateFilterServiceImpl;
 import com.bbva.net.front.controller.GlobalPositionController;
 import com.bbva.net.front.core.AbstractBbvaController;
@@ -166,9 +165,6 @@ public class GlobalPositionControllerImpl extends AbstractBbvaController impleme
 	 */
 	private String cardSelected = StringUtils.EMPTY;
 
-	@Resource(name = "dateFilterService")
-	private transient DateFilterService dateFilterService;
-
 	/**
 	 * @author Entelgy
 	 */
@@ -187,10 +183,8 @@ public class GlobalPositionControllerImpl extends AbstractBbvaController impleme
 		// Get GlobalProductsDTO by currentUser (visibles and hidden)
 		this.globalProductsDTO = this.globalPositionFacade.getGlobalProductsByUser();
 
-		EnumPeriodType periodType = EnumPeriodType.valueOf(EnumPeriodType.LAST_SIX_MONTH.getPeriodId());
-		DateRangeDto dateRange = dateFilterService.getPeriodFilter(periodType);
 		// Obtiene la lista de resumen de movimientos del serivico REST
-		this.globalResumeMovementsDTO = this.movementsResumeFacade.getMovementsResumeByCustomer(dateRange);
+		this.globalResumeMovementsDTO = this.movementsResumeFacade.getMovementsResumeByCustomer(new DateRangeDto());
 
 		// Obtiene la lista de datos para pintar la grafica Deposito electrónico
 		this.globalMonthlyBalance = this.accountMonthBalanceFacade.getAccountMonthlyBalance(globalProductsDTO
