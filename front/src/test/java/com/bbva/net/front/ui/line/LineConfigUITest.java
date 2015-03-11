d3da6ca1de94bd30c075e9b2c800302cfdef9a76
fgparamio <fgparamio@yahoo.es>
2015-03-11 16:51:23 -0500

    Add test coverage

---
+++ front/src/test/java/com/bbva/net/front/ui/line/LineConfigUITest.java
@@ -0,0 +1,12 @@
+package com.bbva.net.front.ui.line;
+
+import com.bbva.net.core.test.AbstractBbvaDTOTest;
+
+public class LineConfigUITest extends AbstractBbvaDTOTest<LineConfigUI> {
+
+	@Override
+	protected LineConfigUI getInstance() {
+		return new LineConfigUI();
+	}
+
+}
---
+++ front/src/test/java/com/bbva/net/front/ui/pie/PieItemUITest.java
@@ -0,0 +1,12 @@
+package com.bbva.net.front.ui.pie;
+
+import com.bbva.net.core.test.AbstractBbvaDTOTest;
+
+public class PieItemUITest extends AbstractBbvaDTOTest<PieItemUI> {
+
+	@Override
+	protected PieItemUI getInstance() {
+		return new PieItemUI();
+	}
+
+}
