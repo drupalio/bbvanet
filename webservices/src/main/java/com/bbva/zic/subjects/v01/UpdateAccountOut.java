52f00b28548f2cee55689adf8a59361d86b01872
Aleja195 <alejandra.gutierrez@entelgy.com>
2015-03-12 16:14:58 -0500

    wadl alias

---
+++ webservices/src/main/java/com/bbva/zic/subjects/v01/UpdateAccountOut.java
@@ -0,0 +1,60 @@
+
+package com.bbva.zic.subjects.v01;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for updateAccountOut complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="updateAccountOut">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="folio" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "updateAccountOut", propOrder = {
+    "folio"
+})
+public class UpdateAccountOut {
+
+    protected String folio;
+
+    /**
+     * Gets the value of the folio property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getFolio() {
+        return folio;
+    }
+
+    /**
+     * Sets the value of the folio property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setFolio(String value) {
+        this.folio = value;
+    }
+
+}
