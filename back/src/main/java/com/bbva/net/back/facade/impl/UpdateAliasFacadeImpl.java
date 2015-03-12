647f87712cb53beff0de0ebfcad257fef31563bb
Aleja195 <alejandra.gutierrez@entelgy.com>
2015-03-12 17:40:48 -0500

    creacion de UpdateAliasFacade.java, UpdateAliasFacadeImpl.java, UpdateAccountDto.java, UpdateAccountDtoTest.java, carpeta subjects en webservices con SubjetsService.java y SubjetsServiceImpl.java, modificacion de: rest.properties y AbstractBbvaRestService.java

---
+++ back/src/main/java/com/bbva/net/back/facade/impl/UpdateAliasFacadeImpl.java
@@ -0,0 +1,17 @@
+package com.bbva.net.back.facade.impl;
+
+import com.bbva.net.back.core.pattern.facade.AbstractBbvaFacade;
+import com.bbva.net.back.facade.UpdateAliasFacade;
+import com.bbva.net.back.model.updateAlias.UpdateAccountDto;
+
+public class UpdateAliasFacadeImpl extends AbstractBbvaFacade implements UpdateAliasFacade {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public UpdateAccountDto updateSubject(String contractNumber, UpdateAccountDto updateAccountDto) {
+
+		return null;
+	}
+
+}
---
+++ webservices/src/main/java/com/bbva/net/webservices/subjects/impl/SubjetsServiceImpl.java
@@ -0,0 +1,20 @@
+package com.bbva.net.webservices.subjects.impl;
+
+import org.apache.cxf.jaxrs.client.WebClient;
+
+import com.bbva.net.webservices.core.pattern.AbstractBbvaRestService;
+import com.bbva.net.webservices.core.stereotype.RestService;
+import com.bbva.net.webservices.subjects.SubjetsService;
+import com.bbva.zic.subjects.v01.UpdateAccountOut;
+import com.bbva.zic.subjects.v01.UpdateSubjectIn;
+
+@RestService(value = "subjetsService")
+public class SubjetsServiceImpl extends AbstractBbvaRestService implements SubjetsService {
+
+	@Override
+	public UpdateAccountOut updateSubject(String contractNumber, UpdateSubjectIn updatesubjectin) {
+		final WebClient webc = getJsonWebClient(URL_SUBJETS);
+		return webc.get(UpdateAccountOut.class);
+	}
+
+}
