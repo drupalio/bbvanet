31b487717e85973231d53f36370f4665ef06e2df
Aleja195 <alejandra.gutierrez@entelgy.com>
2015-04-13 11:36:59 -0500

    CheckBookStatusTest.java, EnumCheckStatusTest.java, ExtractControllerImplTest.java

--- back/src/test/java/com/bbva/net/back/model/comboFilter/CheckBookStatusTest.java
+++ back/src/test/java/com/bbva/net/back/model/comboFilter/CheckBookStatusTest.java
@@ -1,5 +1,6 @@
 package com.bbva.net.back.model.comboFilter;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.bbva.net.core.test.TestUtils;
@@ -12,16 +13,64 @@ public class CheckBookStatusTest {
 	}
 
 	@Test
+	public void checkAttribute() {
+		EnumCheckBookStatus.ANULADO.setStatusId("1");
+		EnumCheckBookStatus.ANULADO.setValue("Anulado");
+		Assert.assertEquals(EnumCheckBookStatus.ANULADO.getStatusId(), "1");
+		Assert.assertEquals(EnumCheckBookStatus.ANULADO.getValue(), "Anulado");
+
+		EnumCheckBookStatus.ANULADO_PETICION.setStatusId("C");
+		EnumCheckBookStatus.ANULADO_PETICION.setValue("Anulado por petición");
+		Assert.assertEquals(EnumCheckBookStatus.ANULADO_PETICION.getStatusId(), "C");
+		Assert.assertEquals(EnumCheckBookStatus.ANULADO_PETICION.getValue(), "Anulado por petición");
+
+		EnumCheckBookStatus.DE_BAJA.setStatusId("B");
+		EnumCheckBookStatus.DE_BAJA.setValue("De baja");
+		Assert.assertEquals(EnumCheckBookStatus.DE_BAJA.getStatusId(), "B");
+		Assert.assertEquals(EnumCheckBookStatus.DE_BAJA.getValue(), "De baja");
+
+		EnumCheckBookStatus.EN_IMPRESOR.setStatusId("I");
+		EnumCheckBookStatus.EN_IMPRESOR.setValue("Impresor");
+		Assert.assertEquals(EnumCheckBookStatus.EN_IMPRESOR.getStatusId(), "I");
+		Assert.assertEquals(EnumCheckBookStatus.EN_IMPRESOR.getValue(), "Impresor");
+
+		EnumCheckBookStatus.EN_OFICINA.setStatusId("O");
+		EnumCheckBookStatus.EN_OFICINA.setValue("OficinaEn");
+		Assert.assertEquals(EnumCheckBookStatus.EN_OFICINA.getStatusId(), "O");
+		Assert.assertEquals(EnumCheckBookStatus.EN_OFICINA.getValue(), "OficinaEn");
+
+		EnumCheckBookStatus.ENTRE_CLIENTE.setStatusId("E");
+		EnumCheckBookStatus.ENTRE_CLIENTE.setValue("Clientes");
+		Assert.assertEquals(EnumCheckBookStatus.ENTRE_CLIENTE.getStatusId(), "E");
+		Assert.assertEquals(EnumCheckBookStatus.ENTRE_CLIENTE.getValue(), "Clientes");
+
+		EnumCheckBookStatus.NO_RECIBIDO.setStatusId("N");
+		EnumCheckBookStatus.NO_RECIBIDO.setValue("No recibido");
+		Assert.assertEquals(EnumCheckBookStatus.NO_RECIBIDO.getStatusId(), "N");
+		Assert.assertEquals(EnumCheckBookStatus.NO_RECIBIDO.getValue(), "No recibido");
+
+		EnumCheckBookStatus.PED_OFICINA.setStatusId("A");
+		EnumCheckBookStatus.PED_OFICINA.setValue("Oficina");
+		Assert.assertEquals(EnumCheckBookStatus.PED_OFICINA.getStatusId(), "A");
+		Assert.assertEquals(EnumCheckBookStatus.PED_OFICINA.getValue(), "Oficina");
+
+		EnumCheckBookStatus.PERDIDO.setStatusId("2");
+		EnumCheckBookStatus.PERDIDO.setValue("Perdido");
+		Assert.assertEquals(EnumCheckBookStatus.PERDIDO.getStatusId(), "2");
+		Assert.assertEquals(EnumCheckBookStatus.PERDIDO.getValue(), "Perdido");
+	}
+
+	@Test
 	public void checkValueOf() {
 		// check Ok
-		EnumCheckBookStatus.valueOfEnum("1");
+		Assert.assertNotNull(EnumCheckBookStatus.valueOfEnum("1"));
 
 	}
 
 	@Test
 	public void mistValueOf() {
 		// check mistake
-		EnumCheckBookStatus.valueOfEnum("5");
+		Assert.assertNull(EnumCheckBookStatus.valueOfEnum("5"));
 	}
 
 }
--- back/src/test/java/com/bbva/net/back/model/comboFilter/EnumCheckStatusTest.java
+++ back/src/test/java/com/bbva/net/back/model/comboFilter/EnumCheckStatusTest.java
@@ -1,5 +1,6 @@
 package com.bbva.net.back.model.comboFilter;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,6 +15,39 @@ public class EnumCheckStatusTest {
 	@Test
 	public void checkEnumCheckEstatus() {
 		TestUtils.enumCodeCoverage(EnumCheckStatus.class);
+	}
+
+	@Test
+	public void checkAttribute() {
+		EnumCheckStatus.DISPONIBLE.setStatusId(0);
+		EnumCheckStatus.DISPONIBLE.setValue("Disponible");
+		Assert.assertEquals(EnumCheckStatus.DISPONIBLE.getStatusId(), 0);
+		Assert.assertEquals(EnumCheckStatus.DISPONIBLE.getValue(), "Disponible");
+
+		EnumCheckStatus.ANULADO.setStatusId(1);
+		EnumCheckStatus.ANULADO.setValue("Anulado");
+		Assert.assertEquals(EnumCheckStatus.ANULADO.getStatusId(), 1);
+		Assert.assertEquals(EnumCheckStatus.ANULADO.getValue(), "Anulado");
+
+		EnumCheckStatus.PERDIDO.setStatusId(2);
+		EnumCheckStatus.PERDIDO.setValue("Perdido");
+		Assert.assertEquals(EnumCheckStatus.PERDIDO.getStatusId(), 2);
+		Assert.assertEquals(EnumCheckStatus.PERDIDO.getValue(), "Perdido");
+
+		EnumCheckStatus.PAGADO_X_CANJE.setStatusId(3);
+		EnumCheckStatus.PAGADO_X_CANJE.setValue("Pagado por Canje");
+		Assert.assertEquals(EnumCheckStatus.PAGADO_X_CANJE.getStatusId(), 3);
+		Assert.assertEquals(EnumCheckStatus.PAGADO_X_CANJE.getValue(), "Pagado por Canje");
+
+		EnumCheckStatus.PAGADO_X_VENTANILLA.setStatusId(4);
+		EnumCheckStatus.PAGADO_X_VENTANILLA.setValue("Pagado por Ventanilla");
+		Assert.assertEquals(EnumCheckStatus.PAGADO_X_VENTANILLA.getStatusId(), 4);
+		Assert.assertEquals(EnumCheckStatus.PAGADO_X_VENTANILLA.getValue(), "Pagado por Ventanilla");
+
+		EnumCheckStatus.ORDEN_NO_PAGO.setStatusId(8);
+		EnumCheckStatus.ORDEN_NO_PAGO.setValue("Orden de no Pago");
+		Assert.assertEquals(EnumCheckStatus.ORDEN_NO_PAGO.getStatusId(), 8);
+		Assert.assertEquals(EnumCheckStatus.ORDEN_NO_PAGO.getValue(), "Orden de no Pago");
 	}
 
 	@Test
