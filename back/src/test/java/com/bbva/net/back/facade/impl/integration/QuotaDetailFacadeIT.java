137bc066711223fb4fefebb99175af322c65c527
Aleja195 <alejandra.gutierrez@entelgy.com>
2015-04-24 17:03:29 -0500

    test de integración

--- back/src/test/java/com/bbva/net/back/facade/impl/integration/QuotaDetailFacadeIT.java
+++ back/src/test/java/com/bbva/net/back/facade/impl/integration/QuotaDetailFacadeIT.java
@@ -1,19 +1,24 @@
 package com.bbva.net.back.facade.impl.integration;
 
+import java.util.Date;
+import java.util.List;
+
 import javax.annotation.Resource;
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.ServiceUnavailableException;
 
 import org.apache.commons.lang.StringUtils;
-import org.junit.runner.RunWith;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.bbva.net.back.facade.QuotaDetailFacade;
+import com.bbva.net.back.model.commons.DateRangeDto;
 import com.bbva.net.back.model.movements.MovementDetailDto;
+import com.bbva.net.back.model.movements.MovementDto;
 import com.bbva.net.back.model.quota.QuotaDetailDto;
 
 @Profile("integration")
@@ -85,4 +90,45 @@ public class QuotaDetailFacadeIT {
 		}
 	}
 
+	// Servicio GetRotaryQuota
+
+	@Test
+	public void checklistRotaryQuotaMovementsOK() throws Exception {
+		final List<MovementDto> quotaDetailDto = this.quotaDetailFacade.listRotaryQuotaMovements(
+				"00130443000200009410", new DateRangeDto(new Date(), new Date()), "098787654", 10);
+		Assert.assertNotNull(quotaDetailDto);
+	}
+
+	@Test(expected = ServiceUnavailableException.class)
+	public void listRotaryQuotaMovementsNotId() throws Exception {
+		try {
+			this.quotaDetailFacade.listRotaryQuotaMovements(null, new DateRangeDto(new Date(), new Date()),
+					"098787654", 10);
+		} catch (final ServiceUnavailableException notFoundException) {
+			Assert.assertEquals(notFoundException.getMessage(), "HTTP 503 Service Unavailable");
+			throw notFoundException;
+		}
+	}
+
+	@Test(expected = ServiceUnavailableException.class)
+	public void listRotaryQuotaMovementsNotPagination() throws Exception {
+		try {
+			this.quotaDetailFacade.listRotaryQuotaMovements("00130443000200009410", new DateRangeDto(new Date(),
+					new Date()), null, 10);
+		} catch (final ServiceUnavailableException notFoundException) {
+			Assert.assertEquals(notFoundException.getMessage(), "HTTP 503 Service Unavailable");
+			throw notFoundException;
+		}
+	}
+
+	@Test(expected = BadRequestException.class)
+	public void listRotaryQuotaMovementsMistake() throws Exception {
+		try {
+			this.quotaDetailFacade.listRotaryQuotaMovements("00130443000200009410", new DateRangeDto(new Date(),
+					new Date()), "0", 10);
+		} catch (final BadRequestException notFoundException) {
+			Assert.assertEquals(notFoundException.getMessage(), "HTTP 400 Bad Request");
+			throw notFoundException;
+		}
+	}
 }
