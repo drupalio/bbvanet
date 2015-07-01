3a7230878ca7e7a6a7627d559f357171f95bc6ea
Aleja195 <alejandra.gutierrez@entelgy.com>
2015-07-01 16:38:31 -0500

    cambios para la operatoria de personalizar

--- front/src/main/webapp/assets/js/newUtil.js
+++ front/src/main/webapp/assets/js/newUtil.js
@@ -233,26 +233,35 @@ $(window).load(function() {
 			$('#modal').remove();
 		}
 	});
+
+	$('#tabsOperations li').each(function(event) {
+		var sdas = $(this).attr('data-origin-value');
+		var div = "<div class='tooltip-arrow' /><div class='tooltip-inner' style='bottom: 0px !important; font-size: 11px !important;'>" + $(this).attr('data-original-title') + "</div>";
+		if (sdas == 'false') {
+			$(this).children('.tab-head').addClass('buttonDisabled');
+
+			$(this).hover(function() {
+				$(this).prepend("<div style='display: block; top: -87px; left: -3px;' id='" + $(this).attr('id') + "tooltip' class='tooltip fade top in' role='tooltip'>" + div + " </div>");
+			}, function() {
+				$(this).children('.tooltip').remove();
+			});
+		}
+	});
 });
 
-
-function localize()
-{
+function localize() {
 	/* Si se puede obtener la localización */
- 	if (navigator.geolocation)
-	{
-        navigator.geolocation.getCurrentPosition(mapa,error);
-    }
-    /* Si el navegador no soporta la recuperación de la geolocalización */
-    else
-    {
-        alert('¡Oops! Tu navegador no soporta geolocalización.');
-    }
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(mapa, error);
+	}
+	/* Si el navegador no soporta la recuperación de la geolocalización */
+	else {
+		alert('¡Oops! Tu navegador no soporta geolocalización.');
+	}
 }
 
-function mapa(pos)
-{
-	/* Obtenemos los parámetros de la API de geolocalización HTML*/
+function mapa(pos) {
+	/* Obtenemos los parámetros de la API de geolocalización HTML */
 	var latitud = pos.coords.latitude;
 	var longitud = pos.coords.longitude;
 	var precision = pos.coords.accuracy;
@@ -260,34 +269,41 @@ function mapa(pos)
 	/* A través del DOM obtenemos el div que va a contener el mapa */
 	var contenedor = document.getElementById("map")
 
-	/* Posicionamos un punto en el mapa con las coordenadas que nos ha proporcionado la API*/
-	var centro = new google.maps.LatLng(latitud,longitud);
+	/*
+	 * Posicionamos un punto en el mapa con las coordenadas que nos ha
+	 * proporcionado la API
+	 */
+	var centro = new google.maps.LatLng(latitud, longitud);
 
 	/* Definimos las propiedades del mapa */
-	var propiedades =
-	{
-        zoom: 15,
-        center: centro,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+	var propiedades = {
+		zoom : 15,
+		center : centro,
+		mapTypeId : google.maps.MapTypeId.ROADMAP
 	};
 
-	/* Creamos el mapa pasandole el div que lo va a contener y las diferentes propiedades*/
+	/*
+	 * Creamos el mapa pasandole el div que lo va a contener y las diferentes
+	 * propiedades
+	 */
 	var map = new google.maps.Map(contenedor, propiedades);
 
-	/* Un servicio que proporciona la API de GM es colocar marcadores sobre el mapa */
+	/*
+	 * Un servicio que proporciona la API de GM es colocar marcadores sobre el
+	 * mapa
+	 */
 	var marcador = new google.maps.Marker({
-        position: centro,
-        map: map,
-        title: "Tu localizacion"
-    });
+		position : centro,
+		map : map,
+		title : "Tu localizacion"
+	});
 }
 
 /* Gestion de errores */
-function error(errorCode)
-{
-	if(errorCode.code == 1)
+function error(errorCode) {
+	if (errorCode.code == 1)
 		alert("No has permitido buscar tu localizacion")
-	else if (errorCode.code==2)
+	else if (errorCode.code == 2)
 		alert("Posicion no disponible")
 	else
 		alert("Ha ocurrido un error")
