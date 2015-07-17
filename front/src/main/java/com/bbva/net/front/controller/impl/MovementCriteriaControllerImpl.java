7b0b29fe0a75f8f1c8f0ed03433e268acfc03cde
Aleja195 <alejandra.gutierrez@entelgy.com>
2015-07-17 09:42:00 -0500

    cambios para ultimos movimientos y validaciones

--- front/src/main/java/com/bbva/net/front/controller/impl/MovementCriteriaControllerImpl.java
+++ front/src/main/java/com/bbva/net/front/controller/impl/MovementCriteriaControllerImpl.java
@@ -171,10 +171,14 @@ public class MovementCriteriaControllerImpl extends MovementPaginatedController 
 		setBalanceRangePc(null);
 		// TODO oroductId
 		setProductTypePc(getSelectedProduct().getSubTypeProd());
-		LOGGER.info("MovementsAccountController getAllMovements productId:  " + getSelectedProduct().getProductId());
-		super.setMovementsFacade(movementsFacade);
-		next();
-		this.movementsList = getCurrentList();
+
+		if (getSelectedProduct().isVisible()) {
+			LOGGER.info("MovementsAccountController getAllMovements productId:  " + getSelectedProduct().getProductId());
+			super.setMovementsFacade(movementsFacade);
+			next();
+			this.movementsList = getCurrentList();
+		}
+
 		this.movementsListGen = this.movementsList;
 		this.graphicLineMovements = graphicLineDelegate.getMovementAccount(this.movementsList);
 		return this.movementsList;
