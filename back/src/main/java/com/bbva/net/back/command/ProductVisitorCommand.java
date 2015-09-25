1b103d81afd048c9d4f1665b76b508702c215aed
Aleja195 <alejandra.gutierrez@entelgy.com>
2015-09-25 17:43:49 -0500

    cambios para el tradeCash (saldo en canje)

--- back/src/main/java/com/bbva/net/back/command/ProductVisitorCommand.java
+++ back/src/main/java/com/bbva/net/back/command/ProductVisitorCommand.java
@@ -8,87 +8,89 @@ import com.bbva.net.core.pattern.VisitorCommand;
 
 public abstract class ProductVisitorCommand extends VisitorCommand<Product> {
 
-	public ProductVisitorCommand(List<Product> list) {
-		super(list);
-	}
+    public ProductVisitorCommand(List<Product> list) {
+        super(list);
+    }
 
-	/************************************ ABSTRACT METHODS **********************************/
+    /************************************ ABSTRACT METHODS **********************************/
 
-	/**
-	 * @param account
-	 */
-	public abstract void executeAccount(final Product account);
+    /**
+     * @param account
+     */
+    public abstract void executeAccount(final Product account);
 
-	/**
-	 * @param adquirenceAccount
-	 */
-	public abstract void executeAdquirenceAccount(final Product adquirenceAccount);
+    /**
+     * @param adquirenceAccount
+     */
+    public abstract void executeAdquirenceAccount(final Product adquirenceAccount);
 
-	/**
-	 * @param product
-	 */
-	public abstract void executeCredictCard(final Product product);
+    /**
+     * @param product
+     */
+    public abstract void executeCredictCard(final Product product);
 
-	/**
-	 * @param product
-	 */
-	public abstract void executeRotatingAccount(final Product product);
+    /**
+     * @param product
+     */
+    public abstract void executeRotatingAccount(final Product product);
 
-	/**
-	 * @param product
-	 */
-	public abstract void executeLeasing(final Product product);
+    /**
+     * @param product
+     */
+    public abstract void executeLeasing(final Product product);
 
-	/**
-	 * @param product
-	 */
-	public abstract void executeLoan(final Product product);
+    /**
+     * @param product
+     */
+    public abstract void executeLoan(final Product product);
 
-	/**
-	 * @param product
-	 */
-	public abstract void executeFund(final Product product);
+    /**
+     * @param product
+     */
+    public abstract void executeFund(final Product product);
 
-	/**
-	 * @param product
-	 */
-	public abstract void executeDeposit(final Product product);
+    /**
+     * @param product
+     */
+    public abstract void executeDeposit(final Product product);
 
-	/**
-	 * 
-	 */
-	@Override
-	protected void execute(Product product) {
+    /**
+     *
+     */
+    @Override
+    protected void execute(Product product) {
 
-		switch (ProductUtils.getEnumProductTypeBySubType(product.getType())) {
-		case PC:
-			this.executeAccount(product);
-			break;
-		case AQ:
-			this.executeAdquirenceAccount(product);
-			break;
-		case TC:
-			this.executeCredictCard(product);
-			break;
-		case RQ:
-			this.executeRotatingAccount(product);
-			break;
-		case LI:
-			this.executeLeasing(product);
-			break;
-		case LO:
-			this.executeLoan(product);
-			break;
-		case SI:
-			this.executeFund(product);
-			break;
-		case ED:
-			this.executeDeposit(product);
-			break;
-		default:
-			throw new RuntimeException();
-		}
-
-	}
+        if (product.getType() != null) {
+            
+            switch (ProductUtils.getEnumProductTypeBySubType(product.getType())) {
+                case PC:
+                    this.executeAccount(product);
+                    break;
+                case AQ:
+                    this.executeAdquirenceAccount(product);
+                    break;
+                case TC:
+                    this.executeCredictCard(product);
+                    break;
+                case RQ:
+                    this.executeRotatingAccount(product);
+                    break;
+                case LI:
+                    this.executeLeasing(product);
+                    break;
+                case LO:
+                    this.executeLoan(product);
+                    break;
+                case SI:
+                    this.executeFund(product);
+                    break;
+                case ED:
+                    this.executeDeposit(product);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 
 }
--- back/src/main/java/com/bbva/net/back/utils/ProductUtils.java
+++ back/src/main/java/com/bbva/net/back/utils/ProductUtils.java
@@ -6,63 +6,63 @@ import java.util.List;
 import com.bbva.czic.dto.net.EnumProductType;
 
 public final class ProductUtils {
-
-	private ProductUtils() {
-	}
-
-	public static EnumProductType getEnumProductTypeBySubType(final String subType) {
-
-		String cuentasVect[] = { "CC", "AH", "CT", "CD", "CE" };
-		List<String> cuentasList = Arrays.asList(cuentasVect);
-
-		String cupoVect[] = { "CR" };
-		List<String> cupoList = Arrays.asList(cupoVect);
-
-		String foundsVect[] = { "FA", "BD", "BF", "PA", "BP", "FN", "FC", "FE", "FZ", "AN", "FE", "FG", "MD", "FD",
-				"FT", "CO", "CF", "FR", "FB" };
-		List<String> foundList = Arrays.asList(foundsVect);
-
-		String pensionesVect[] = { "FP" };
-		List<String> pensionesList = Arrays.asList(pensionesVect);
-
-		String loanVect[] = { "HI", "CS", "CM", "MC", "CL", "J1", "J2" };
-		List<String> loanList = Arrays.asList(loanVect);
-
-		String cardVect[] = { "TC", "TE", "TO" };
-		List<String> cardList = Arrays.asList(cardVect);
-
-		String leasingVect[] = { "LS" };
-		List<String> leasingList = Arrays.asList(leasingVect);
-
-		// ToDo: Deposito Electronico
-		String depositElecVect[] = { "DE" };
-		List<String> depositElecList = Arrays.asList(depositElecVect);
-
-		// ToDo:adquisición
-		String adqVect[] = { "AQ" };
-		List<String> adqList = Arrays.asList(adqVect);
-
-		if (cuentasList.contains(subType)) {
-			return EnumProductType.PC;
-		} else if (cupoList.contains(subType)) {
-			return EnumProductType.RQ;
-		} else if (foundList.contains(subType)) {
-			return EnumProductType.SI;
-		} else if (pensionesList.contains(subType)) {
-			return null;
-		} else if (loanList.contains(subType)) {
-			return EnumProductType.LO;
-		} else if (cardList.contains(subType)) {
-			return EnumProductType.TC;
-		} else if (leasingList.contains(subType)) {
-			return EnumProductType.LI;
-		} else if (depositElecList.contains(subType)) {
-			return EnumProductType.ED;
-		} else if (adqList.contains(subType)) {
-			return EnumProductType.AQ;
-		}
-
-		return null;
-	}
-
+    
+    private ProductUtils() {
+    }
+    
+    public static EnumProductType getEnumProductTypeBySubType(final String subType) {
+        
+        String cuentasVect[] = { "CC", "AH", "CT", "CD", "CE" };
+        List<String> cuentasList = Arrays.asList(cuentasVect);
+        
+        String cupoVect[] = { "CR" };
+        List<String> cupoList = Arrays.asList(cupoVect);
+        
+        String foundsVect[] = { "FA", "BD", "BF", "PA", "BP", "FN", "FC", "FE", "FZ", "AN", "FE", "FG", "MD", "FD",
+                "FT", "CO", "CF", "FR", "FB" };
+        List<String> foundList = Arrays.asList(foundsVect);
+        
+        String pensionesVect[] = { "FP" };
+        List<String> pensionesList = Arrays.asList(pensionesVect);
+        
+        String loanVect[] = { "HI", "CS", "CM", "MC", "CL", "J1", "J2" };
+        List<String> loanList = Arrays.asList(loanVect);
+        
+        String cardVect[] = { "TC", "TE", "TO" };
+        List<String> cardList = Arrays.asList(cardVect);
+        
+        String leasingVect[] = { "LS" };
+        List<String> leasingList = Arrays.asList(leasingVect);
+        
+        // ToDo: Deposito Electronico
+        String depositElecVect[] = { "DE" };
+        List<String> depositElecList = Arrays.asList(depositElecVect);
+        
+        // ToDo:adquisición
+        String adqVect[] = { "AQ" };
+        List<String> adqList = Arrays.asList(adqVect);
+        
+        if (cuentasList.contains(subType)) {
+            return EnumProductType.PC;
+        } else if (cupoList.contains(subType)) {
+            return EnumProductType.RQ;
+        } else if (foundList.contains(subType)) {
+            return EnumProductType.SI;
+        } else if (pensionesList.contains(subType)) {
+            return EnumProductType.N;
+        } else if (loanList.contains(subType)) {
+            return EnumProductType.LO;
+        } else if (cardList.contains(subType)) {
+            return EnumProductType.TC;
+        } else if (leasingList.contains(subType)) {
+            return EnumProductType.LI;
+        } else if (depositElecList.contains(subType)) {
+            return EnumProductType.ED;
+        } else if (adqList.contains(subType)) {
+            return EnumProductType.AQ;
+        }
+        
+        return null;
+    }
+    
 }
