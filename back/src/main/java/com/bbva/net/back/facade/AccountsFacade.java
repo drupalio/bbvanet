abde0e1809aa12614a00927df54775c98de3b564
Aleja195 <alejandra.gutierrez@entelgy.com>
2015-11-17 14:13:29 -0500

    cambios favoritos

--- back/src/main/java/com/bbva/net/back/facade/AccountsFacade.java
+++ back/src/main/java/com/bbva/net/back/facade/AccountsFacade.java
@@ -1,39 +1,43 @@
 package com.bbva.net.back.facade;
 
-import java.util.List;
+// <!-- Entelgy / SPRING 3 / 17112015 / INICIO -->
 import java.util.Map;
-
-import com.bbva.net.back.model.globalposition.AccountDto;
 import com.bbva.net.back.model.globalposition.AdquirenceAccountDto;
 import com.bbva.net.back.model.globalposition.BalanceDto;
 import com.bbva.net.back.model.globalposition.GlobalProductsDto;
+// <!-- Entelgy / SPRING 3 / 17112015 / FIN -->
+
+import java.util.List;
+import com.bbva.net.back.model.globalposition.AccountDto;
 
 /**
  * @author Entelgy
  */
 public interface AccountsFacade {
-    
+
+    // <!-- Entelgy / SPRING 3 / 17112015 / INICIO -->
     /**
-     * @param user
+     * @param globalProductsDTO
      * @return
      */
     List<AccountDto> getAccountsByUser(final GlobalProductsDto globalProductsDTO);
-    
+
     /**
-     * @param user
+     * @param globalProductsDTO
      * @return
      */
     List<AccountDto> getAccountsByUserHidden(final GlobalProductsDto globalProductsDTO);
-    
+
     /**
      * @param globalProducts
      * @return
      */
     Map<String, BalanceDto> getAccountsTotals(final GlobalProductsDto globalProducts);
-    
+
     /**
      * @param globalProductsDTO
      * @return
      */
     List<AdquirenceAccountDto> getAccountsByUserAqui(final GlobalProductsDto globalProductsDTO);
+    // <!-- Entelgy / SPRING 3 / 17112015 / FIN -->
 }
--- back/src/main/java/com/bbva/net/back/mapper/impl/FavoriteOperationsMapperImpl.java
+++ back/src/main/java/com/bbva/net/back/mapper/impl/FavoriteOperationsMapperImpl.java
@@ -21,52 +21,41 @@ import com.bbva.zic.agileoperations.v01.AgileOperation;
  */
 @Mapper(value = "favoriteOperationsMapper")
 public class FavoriteOperationsMapperImpl extends ConfigurableMapper implements FavoriteOperationsMapper {
-
-    // <!-- Entelgy / GP13137 / 22102015 / INICIO -->
+    
+    // <!-- Entelgy / GP13137 / 17112015 / INICIO -->
     /**
      *
      */
     @Override
     protected void configure(final MapperFactory factory) {
-
+        
         factory.getConverterFactory().registerConverter(new MoneyConverter());
-        factory.getConverterFactory().registerConverter(new StringToDateConverter("yyyy-MM-dd HH:MM:SS"));
-
+        factory.getConverterFactory().registerConverter(new StringToDateConverter("yyyy-MM-dd"));
+        
         // Map AgileOperation to FavoriteOperationDto
         factory.classMap(AgileOperation.class, FavoriteOperationDto.class).field("contractId", "contractId")
-        .field("amount", "amount").field("id", "idOperation").field("name", "name")
-        .field("transactionDate", "transactionDate").field("transactionReference", "transactionReference")
-        .field("origin.cardNumber.number", "numberOrigin").field("origin.ccc.bankCode", "bankCodeOrigin")
-        .field("origin.ccc.branchCode", "branchCodeOrigin").field("origin.ccc.controlDigit", "controlDigitOrigin")
-        .field("origin.ccc.accountNumber", "accountNumberOrigin").field("origin.clabe.bankCode", "bankCodeOrigin")
-        .field("origin.clabe.regionCode", "regionCodeOrigin").field("origin.clabe.accountNumber", "accountNumberOrigin")
-        .field("origin.clabe.controlDigit", "controlDigitOrigin").field("origin.mobilePhoneNumber.number", "numberOrigin")
-        .field("origin.creditNumber.number", "numberOrigin").field("destination.cardNumber.number", "numberDest")
-        .field("destination.ccc.bankCode", "bankCodeDest").field("destination.ccc.branchCode", "branchCodeDest")
-        .field("destination.ccc.controlDigit", "controlDigitDest").field("destination.ccc.accountNumber", "accountNumberDest")
-        .field("destination.clabe.bankCode", "bankCodeDest").field("destination.clabe.regionCode", "regionCodeDest")
-        .field("destination.clabe.accountNumber", "accountNumberDest").field("destination.clabe.controlDigit", "controlDigitDest")
-        .field("destination.mobilePhoneNumber.number", "numberDest").field("destination.creditNumber.number", "numberDest")
-        .byDefault().register();
+                .field("amount", "amount").field("id", "idOperation").field("name", "name")
+                .field("transactionDate", "transactionDate").field("transactionReference", "transactionReference")
+                .byDefault().register();
     }
-
+    
     @Override
     public AgileOperation map(FavoriteOperationDto favOperation) {
         AgileOperation agileOperation = map(favOperation, AgileOperation.class);
         return agileOperation;
     }
-
+    
     /**
      * Mapper of Favorite Operations
      */
     @Override
     public List<FavoriteOperationDto> map(final List<AgileOperation> favOperations) {
         final List<FavoriteOperationDto> favOperationsDto = new ArrayList<FavoriteOperationDto>();
-
+        
         new VisitorCommand<AgileOperation>(favOperations) {
-
+            
             FavoriteOperationDto fav;
-
+            
             @Override
             public void execute(final AgileOperation operation) {
                 fav = new FavoriteOperationDto();
@@ -78,7 +67,7 @@ public class FavoriteOperationsMapperImpl extends ConfigurableMapper implements 
         };
         return favOperationsDto;
     }
-
+    
     /**
      * Asigna tipo de origen
      *
@@ -104,7 +93,7 @@ public class FavoriteOperationsMapperImpl extends ConfigurableMapper implements 
         }
         return tipoOrigen;
     }
-
+    
     /**
      * Asigna tipo de destino
      *
@@ -130,5 +119,5 @@ public class FavoriteOperationsMapperImpl extends ConfigurableMapper implements 
         }
         return tipoDestination;
     }
-    // <!-- Entelgy / GP13137 / 22102015 / FIN -->
+    // <!-- Entelgy / GP13137 / 17112015 / FIN -->
 }
