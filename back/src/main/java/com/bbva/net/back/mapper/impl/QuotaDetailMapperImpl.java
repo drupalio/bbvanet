6a8312376e8af79d5194505704ca71a26ae5ee65
Aleja195 <alejandra.gutierrez@entelgy.com>
2015-12-23 14:11:11 -0500

    cambio balance Loan

--- back/src/main/java/com/bbva/net/back/mapper/impl/QuotaDetailMapperImpl.java
+++ back/src/main/java/com/bbva/net/back/mapper/impl/QuotaDetailMapperImpl.java
@@ -18,55 +18,57 @@ import com.bbva.net.back.model.quota.QuotaDetailDto;
 
 @Mapper(value = "quotaDetailMapper")
 public class QuotaDetailMapperImpl extends ConfigurableMapper implements QuotaDetailMapper {
-
-	/**
-	 *
-	 */
-	@Override
-	protected void configure(MapperFactory factory) {
-
-		factory.getConverterFactory().registerConverter(new MoneyConverter());
-		// Map Loan QuotaDetailDto
-
-		factory.getConverterFactory().registerConverter(new StringToDateConverter("yyyy-MM-dd"));
-
-		factory.classMap(Loan.class, QuotaDetailDto.class).field("payment.numbersOfQuota", "numberOfShares")
-				.field("id", "id").field("payment.shortDate", "datePrevious").field("payment.dueDate", "dateMaturity")
-				.field("payment.paymentDate", "datePayment").field("status", "state")
-				.field("payment.minimumPayment", "minimumPayment").field("payment.fees", "feeCollection")
-				.field("balance.availableBalance", "availableBalance").field("balance.total", "balancePrevious")
-				.field("debt.availableBalance", "outstandingBalance").field("debt.total", "amountRequested")
-				.byDefault().register();
-
-		factory.classMap(RotaryQuotaMove.class, MovementDetailDto.class).field("id", "id").field("concept", "concept")
-				.field("transactionDate", "operationDate").field("deb.availableBalance", "valueslope")
-				.field("numberOfQuotas", "numbersOfQuota").field("remainingQuotas", "remainingQuotas").byDefault()
-				.register();
-
-		factory.classMap(Movement.class, MovementDto.class).field("id", "movementId").field("numExt", "extractNumber")
-				.field("concept", "movementConcept").field("transactionDate", "movementDate")
-				.field("operation.code", "movementDetailDto.operationCode")
-				.field("operation.description", "movementDetailDto.operationDescription").field("status", "status")
-				.field("value", "movementValue").field("balance", "totalBalance").byDefault().register();
-
-	}
-
-	@Override
-	public QuotaDetailDto mapQuota(final Loan loan) {
-		final QuotaDetailDto quotaDetailDto = map(loan, QuotaDetailDto.class);
-		return quotaDetailDto;
-	}
-
-	@Override
-	public MovementDetailDto mapQuotaMove(final RotaryQuotaMove rotaryQuotaMove) {
-		final MovementDetailDto quotaMoveDetailDto = map(rotaryQuotaMove, MovementDetailDto.class);
-		return quotaMoveDetailDto;
-
-	}
-
-	@Override
-	public List<MovementDto> listRotaryQuotaMovements(final List<Movement> movement) {
-		final List<MovementDto> quotaMovementsDto = mapAsList(movement, MovementDto.class);
-		return quotaMovementsDto;
-	}
+    
+    /**
+     *
+     */
+    @Override
+    protected void configure(MapperFactory factory) {
+        
+        factory.getConverterFactory().registerConverter(new MoneyConverter());
+        // Map Loan QuotaDetailDto
+        
+        factory.getConverterFactory().registerConverter(new StringToDateConverter("yyyy-MM-dd"));
+        
+        // <!-- Entelgy / Sprint 3 / 23122015 / INICIO -->
+        factory.classMap(Loan.class, QuotaDetailDto.class).field("payment.numbersOfQuota", "numberOfShares")
+                .field("id", "id").field("payment.shortDate", "datePrevious").field("payment.dueDate", "dateMaturity")
+                .field("payment.paymentDate", "datePayment").field("status", "state")
+                .field("payment.minimumPayment", "minimumPayment").field("payment.fees", "feeCollection")
+                .field("balanceloan.availableBalance", "availableBalance").field("balanceloan.total", "balancePrevious")
+                .field("debt.availableBalance", "outstandingBalance").field("debt.total", "amountRequested")
+                .byDefault().register();
+        // <!-- Entelgy / Sprint 3 / 23122015 / FIN -->
+        
+        factory.classMap(RotaryQuotaMove.class, MovementDetailDto.class).field("id", "id").field("concept", "concept")
+                .field("transactionDate", "operationDate").field("deb.availableBalance", "valueslope")
+                .field("numberOfQuotas", "numbersOfQuota").field("remainingQuotas", "remainingQuotas").byDefault()
+                .register();
+                
+        factory.classMap(Movement.class, MovementDto.class).field("id", "movementId").field("numExt", "extractNumber")
+                .field("concept", "movementConcept").field("transactionDate", "movementDate")
+                .field("operation.code", "movementDetailDto.operationCode")
+                .field("operation.description", "movementDetailDto.operationDescription").field("status", "status")
+                .field("value", "movementValue").field("balance", "totalBalance").byDefault().register();
+                
+    }
+    
+    @Override
+    public QuotaDetailDto mapQuota(final Loan loan) {
+        final QuotaDetailDto quotaDetailDto = map(loan, QuotaDetailDto.class);
+        return quotaDetailDto;
+    }
+    
+    @Override
+    public MovementDetailDto mapQuotaMove(final RotaryQuotaMove rotaryQuotaMove) {
+        final MovementDetailDto quotaMoveDetailDto = map(rotaryQuotaMove, MovementDetailDto.class);
+        return quotaMoveDetailDto;
+        
+    }
+    
+    @Override
+    public List<MovementDto> listRotaryQuotaMovements(final List<Movement> movement) {
+        final List<MovementDto> quotaMovementsDto = mapAsList(movement, MovementDto.class);
+        return quotaMovementsDto;
+    }
 }
